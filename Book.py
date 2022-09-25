499c48779bc231ccf2eca0419e418248f444440d
Kurgan <alexei@gmail.com>
2022-09-25 16:19:46 +0300

    Added file author.py

--- Book.py
+++
@@ -1,7 +0,0 @@
-age = int(input('\nВведите ваш возраст: '))
-height = int(input('Введите ваш рост: '))
-weight = int(input('Введите ваш вес: '))
-
-workout = int(age / 5)
-
-print('\nДля вас оптимально заниматься', workout, 'раз в неделю')
