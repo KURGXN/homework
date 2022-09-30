0c1783c8088e1a1b84ed82c9a17f306af560aa7a
zapol <kurgxn@gmail.com>
2022-09-30 20:33:01 +0300

    Всё работает

--- task-3.py
+++ task-3.py
@@ -1,13 +1,26 @@
 print('Задача 3. Улучшенная лингвистика')
 
 words_list = []
+word_count_list = [0, 0, 0]
 print()
 
 for count in range(3):
     words_list.append(input(f'Введите {count + 1} слово: '))
 
-while True:
-    word = input('Слово из текста: ')
+new_word = ''
+print()
 
-#
-# print(f'\nПодсчёт слов в тексте\n{word1}:{word1_count}\n{word2}:{word2_count}\n{word3}:{word3_count}')
+while new_word != 'end':
+    new_word = input('Слово из текста: ')
+    if new_word == words_list[0]:
+        word_count_list[0] += 1
+    elif new_word == words_list[1]:
+        word_count_list[1] += 1
+    elif new_word == words_list[2]:
+        word_count_list[2] += 1
+    elif new_word == 'end':
+        break
+
+print(f'\nПодсчёт слов в тексте')
+for number in range(3):
+    print(f'{words_list[number]}:{word_count_list[number]}')
