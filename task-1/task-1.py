1aef41c1022c7be1272a4a7f9d50dd58c8ba4a63
zapol <kurgxn@gmail.com>
2022-09-30 15:40:33 +0300

    task-1.py Eddited

--- task-1/task-1.py
+++ task-1/task-1.py
@@ -1,9 +1,11 @@
 print('Задача 1. Гугл')
 
 nums_list = []
+nums_count = int(input('\nКол-во чисел в списке: '))
 
-for _ in range(int(input('\nКол-во чисел в списке: '))):
-    nums_list.append(int(input('Введите число: ')))
+for _ in range(nums_count):
+    new_num = int(input('Введите число: '))
+    nums_list.append(new_num)
 
 print('\nМаксимальное число в списке:', max(nums_list))
 print('Минимальное число в списке:', min(nums_list))
