5e90c65203d80e31027a03380e6a0bc4d8fa085d
zapol <kurgxn@gmail.com>
2022-09-30 14:50:19 +0300

    task-3.py is working

--- task-3/task-3.py
+++ task-3/task-3.py
@@ -1,3 +1,23 @@
 print('Задача 3. Контроль')
 
 
+def employee_finder():
+    global employees
+    need_id = int(input('\nКакой ID ищем? '))
+    for employee in employees:
+        if employee == need_id:
+            print('Сотрудник на месте!')
+            return
+    print('Сотрудник не работает!')
+
+
+employees = []
+employees_count = int(input('\nКол-во сотрудников в офисе: '))
+if employees_count < 5:
+    print('Похоже сегодня день отдыха) Даже пяти не набралось\n')
+
+for _ in range(employees_count):
+    ID = int(input('ID сотрудника: '))
+    employees.append(ID)
+
+employee_finder()
